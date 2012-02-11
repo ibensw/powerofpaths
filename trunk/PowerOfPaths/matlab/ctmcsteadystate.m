6834a9cab2e2e7384d941a5ff3a4749b357162de
wouter.ibens <wouter.ibens@afdca923-79cc-7d1e-4922-d7cacc6d481f>
2012-02-11 23:53:55 +0000

    Thesis started

--- trunk/PowerOfPaths/matlab/ctmcsteadystate.m
+++ trunk/PowerOfPaths/matlab/ctmcsteadystate.m
@@ -1,19 +1,19 @@
 function [ pi ] = ctmcsteadystate( Q )
 %CTMCSTEADYSTATE Steady state distribution of a continious time markov chain
 %Source: http://speed.cis.nctu.edu.tw/~ydlin/course/cn/nsd2009/Markov-chain.pdf (slide 10)
-%	T=Q;
-%	len=length(Q);
-%	T(:,len)=ones(len, 1);
-%	e=zeros(1, len);
-%	e(len)=1;
-%	pi=e*inv(T);
+	T=Q;
+	len=length(Q);
+	T(:,len)=ones(len, 1);
+	e=zeros(1, len);
+	e(len)=1;
+	pi=e*inv(T);
 
 %better version, works w/o inverse so faster for sparse matrices
-	len=length(Q);
-	T=[Q ones(len, 1); zeros(1, len+1)];
-	z=zeros(1, len+1);
-	z(len+1)=1;
-	pi=mrdivide(z,T);
+%	len=length(Q);
+%	T=[Q ones(len, 1); zeros(1, len+1)];
+%	z=zeros(1, len+1);
+%	z(len+1)=1;
+%	pi=mrdivide(z,T);
 	%pi=cs_cholsol(T',z')';
 end
 
