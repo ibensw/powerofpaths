69195b1ef96f46c83cb44285df4eb0193235b181
wouter.ibens <wouter.ibens@afdca923-79cc-7d1e-4922-d7cacc6d481f>
2012-03-10 14:09:32 +0000

    makefile

--- trunk/PowerOfPaths/Debug/src/simulator/schedule.d
+++
@@ -1,7 +0,0 @@
-src/simulator/schedule.d src/simulator/schedule.o: \
- ../src/simulator/schedule.cpp ../src/simulator/schedule.h \
- ../src/simulator/event.h
-
-../src/simulator/schedule.h:
-
-../src/simulator/event.h:
--- trunk/PowerOfPaths/Debug/src/simulator/simulator.d
+++
@@ -1,9 +0,0 @@
-src/simulator/simulator.d src/simulator/simulator.o: \
- ../src/simulator/simulator.cpp ../src/simulator/simulator.h \
- ../src/simulator/event.h ../src/simulator/schedule.h
-
-../src/simulator/simulator.h:
-
-../src/simulator/event.h:
-
-../src/simulator/schedule.h:
