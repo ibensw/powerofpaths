e6431cb61e40ae56c8530db71fd3eb5b5ca78599
Wouter ibens <ibensw@users.noreply.github.com>
2012-03-10 14:09:32 +0000

    makefile

--- PowerOfPaths/Debug/src/ring/node.d
+++
@@ -1,3 +0,0 @@
-src/ring/node.d src/ring/node.o: ../src/ring/node.cpp ../src/ring/node.h
-
-../src/ring/node.h:
--- PowerOfPaths/Debug/src/schedule.d
+++
@@ -1,6 +0,0 @@
-src/schedule.d src/schedule.o: ../src/schedule.cpp ../src/schedule.h \
- ../src/event.h
-
-../src/schedule.h:
-
-../src/event.h:
