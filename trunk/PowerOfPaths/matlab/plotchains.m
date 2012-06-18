fde93d092b96979e42b04508b909c1b307417604
wouter.ibens <wouter.ibens@afdca923-79cc-7d1e-4922-d7cacc6d481f>
2012-06-18 21:32:41 +0000

    presentatie

--- trunk/PowerOfPaths/matlab/plotchains.m
+++ trunk/PowerOfPaths/matlab/plotchains.m
@@ -1,4 +1,4 @@
-l=0.10:0.10:1;
+l=0.05:0.05:1;
 s=10;
 r=zeros(1, length(l));
 rs=r;
@@ -11,24 +11,13 @@ lr=r;
 j=1;
 for i=l
 	fprintf('%f ', i);
-%	r(j)=avghops(rightchain(s, i), 0);
-%	rs(j)=avghops(randswitchchain(s, i), 0);
-%	rp(j)=avghops(rprimechain(s, i), 0);
 	ru(j)=ruavghops(runvisitedchain(s, i), 0);
 
 	lr(j)=lumpavghops(rightchain(s, i));
-%	lrs(j)=lumpavghops(randswitchchain(s, i));
+	lrs(j)=lumpavghops(randswitchchain(s, i));
 	lrp(j)=lumpavghops(rprimechain(s, i));
 	j=j+1;
 end
 fprintf('\n');
 
-%d=lrs(1);
-%e=lrs(length(lrs));
-%a=4*(d-e);
-%f=a.*l.*l-a.*l+d;
-
-%plot(l, lr)
-%plot(l, lr./lr, l, lrp./lr);
-%plot(l, r./r, l, rs./r, l, rp./r, l, ru./r);
 plot(l, lr./lr, l, lrp./lr, l, ru./lr);
